28229ff7c9e56420fd1ffa5917b439f528a3ed76
supreme8888 <61455911+supreme8888@users.noreply.github.com>
2020-10-14 00:23:08 +0300

    Update task5.groovy

--- tasks/task5.groovy
+++ tasks/task5.groovy
@@ -1,6 +1,11 @@
+def int isIp(string) {
+
 String.metaClass.isIp={
    if(org.codehaus.groovy.grails.validation.routines.InetAddressValidator.getInstance().isValidInet4Address(delegate)){
     return true;
    } else {
     return false;
     } 
+}
+}
+return this
