e3deac26d2e7114abbff447a20137ed6b99b79ae
Valiantsin Ratomski <Valiantsin_Ratomski@epam.com>
2020-10-14 02:09:39 +0300

    added tasks

--- tasks/task5.groovy
+++ tasks/task5.groovy
@@ -1,7 +1,7 @@
 def isIp(ip) {
         ip = ip.split('\\.', -1)
-	String new_ip = ip
-        if(Eval.me(new_ip).size == 4)
+	    new_ip = (List)ip
+        if(new_ip.size == 4)
                 return true
         else
                 return false
