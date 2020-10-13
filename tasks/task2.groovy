fe7381ad70852d2ae9e215abf4d1e8723b7623a8
supreme8888 <61455911+supreme8888@users.noreply.github.com>
2020-10-13 23:20:46 +0300

    Update task2.groovy

--- tasks/task2.groovy
+++ tasks/task2.groovy
@@ -1,3 +1,14 @@
 """
 comment
 """
+  def mulEvenOdd(my_list){
+      	
+      for(int i in my_list) {
+         List res_list = []
+         my_res = i % 2 == 0 ? i* 2 : i * 3
+         res_list << my_res
+      }
+	return(res_list)	
+   }
+
+return this
