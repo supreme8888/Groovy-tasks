2fcc3adfdf7e89f3cf5f6fca3dbc14f179201d79
Alexey Afanasenko <Alexey_Afanasenko@epam.com>
2020-10-13 15:44:26 +0300

    add solved task8

---
+++ tasks/task8.groovy
@@ -0,0 +1,10 @@
+import groovy.json.JsonSlurper
+def parseAndFilterJson(string) {
+  jsonSlurper = new JsonSlurper()
+  def obj = jsonSlurper.parseText(string)
+  obj = obj.findAll{ it.value % 9 == 0}
+  def builder = new groovy.json.JsonBuilder(obj)
+  return builder.toPrettyString()
+}
+
+return this
