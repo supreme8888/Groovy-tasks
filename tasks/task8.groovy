e529258f05e516fb4269b5a94ac4c356cbccc922
Ilya Melnik <Ilya_Melnik1@epam.com>
2020-10-13 22:23:27 +0300

    newfix3

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -12,7 +12,7 @@ def parseAndFilterJson(string) {
             newMap.put(entry.key, entry.value)
         }
     }
-    def json = JsonOutput.toJson(newMap).toString()
+    def json = JsonOutput.toJson(newMap)
 }
 
-println(parseAndFilterJson("{\"Kate\":18,\"Alan\":16,\"Osvald\":27}"))
+return this
