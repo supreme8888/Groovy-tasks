4fcc8a756b2915abea680d054658fcb64561ae47
Alexey Afanasenko <Alexey_Afanasenko@epam.com>
2020-10-13 20:34:50 +0300

    add solved task11

---
+++ tasks/task11.groovy
@@ -0,0 +1,11 @@
+def gstring(string, int n) {
+  res = [ "1()", "2()", "3()"]
+  for (i = 0; i < res.size; i++) {
+    if(i == n - 1) {
+      res[i] = "${n}(${string})"
+    }
+  }
+  return res
+}
+
+return this
