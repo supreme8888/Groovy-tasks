ec7f92dd64753e9eb02c50517991394f002a0cb1
kanstantsin_karpau <Kanstantsin_Karpau@epam.com>
2020-10-14 01:07:39 +0300

    add solution for task4

---
+++ tasks/task4.groovy
@@ -0,0 +1,13 @@
+// def fib(n) { n < 2 ? n : fib(n - 2) + fib(n - 1) }
+def fib = {
+    int n->
+    def n1 = 0, n2 = 1;
+      n.times { 
+       (n1, n2) = [n2, n1 + n2] 
+      }
+     return n1 
+}
+println (fib(10))
+
+return this
+
