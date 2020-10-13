ef66a28ce3b348dea39f2cee1dede27d0ca0d11f
Alexey Afanasenko <Alexey_Afanasenko@epam.com>
2020-10-13 18:51:58 +0300

    add solved task7

--- tasks/task7.groovy
+++ tasks/task7.groovy
@@ -2,7 +2,11 @@ def encryptThis(string) {
   string=string.trim()
   String[] str;
   str = string.split(' ');
-  str = str.collect {it = (int) it[0] + it.substring(1).reverse()}
+  str = str.collect {
+    fl = it.substring(1, 2)
+    ll = it.substring(it.length() - 1)
+    it = (int) it[0] + ll + it.substring(2,it.length() - 1) + fl
+    }
   String result = str.join(' ').trim()
   return result.toString()  
 }
