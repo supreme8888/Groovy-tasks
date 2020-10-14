f4245175f4545540e05fd9281674af467d31407e
supreme8888 <61455911+supreme8888@users.noreply.github.com>
2020-10-14 16:39:35 +0300

    Create task7.groovy

---
+++ tasks/task7.groovy
@@ -0,0 +1,7 @@
+def encryptThis(string){
+    string = string.split(" ")
+    def finalres = " "
+        for (i in string)
+        { finalres = finalres + (int)i[0] + i[-1] + i[2..-2] + i[1] + " "} 
+    return finalres
+}
