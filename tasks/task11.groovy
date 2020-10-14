d5015cc0b6fcadcd36dfea4cfadd13c06b0c1a18
sshevtsov <sergei_shevtsov@epam.com>
2020-10-14 23:33:38 +0300

    task11

--- tasks/task11.groovy
+++ tasks/task11.groovy
@@ -1,11 +1,12 @@
-def gstring(s, n)
+def gstring(s, int n)
 {
-  def_s="1() 2() 3()"
+ def_s="1() 2() 3()"
  StringBuffer sb = new StringBuffer(def_s);
 
-poin=def_s.indexOf("$n")
-
-return(sb.insert(poin+2,"$s"))
+ poin=def_s.indexOf("$n")
+ new_st=sb.insert(poin+2,"$s")
+ t="$new_st"
+ return(t.trim())
 
 }
 return this
