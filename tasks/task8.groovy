551966d61c04ba9faaffaeda0e9ccca320796d8b
sshevtsov <sergei_shevtsov@epam.com>
2020-10-14 22:33:25 +0300

    task8

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -1,11 +1,14 @@
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
 
 def parseAndFilterJson(j)
 {
 
   def jsonSlurper = new JsonSlurper()
   def object = jsonSlurper.parseText(j)
-  return(object.findAll{it.value % 9 == 0})
+  new_j=object.findAll{it.value % 9 == 0}
+  def output = JsonOutput.toJson(new_j)
+  return(output)
 
 }
 
