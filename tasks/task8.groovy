6514309379fecce9667f50e979da4faa0a51958c
Dzmitry_Kramich <Dzmitry_Kramich@epam.com>
2020-10-14 23:13:47 +0300

    day1-day2

--- tasks/task8.groovy
+++ tasks/task8.groovy
@@ -1,8 +1,8 @@
-import groovy.json.*
+import groovy.json.JsonSlurper
 
 def parseAndFilterJson(string) {
   def jsonSlurper = new JsonSlurper().parseText(string)
-  def JsonBuilder = new groovy.json.JsonBuilder() 
+  def JsonBuilder = new groovy.json.JsonBuilder(string) 
   parseJson = JsonBuilder(jsonSlurper.findAll{ it.value % 9 == 0 })
   return parseJson.toString()
 }
