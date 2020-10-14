51eb5f4e80e6df2f82f73c6cc21419a35ae3ad58
shnipa <nikitashnipov@gmail.com>
2020-10-14 22:43:46 +0300

    8

---
+++ tasks/task8.groovy
@@ -0,0 +1,14 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+def parseAndFilterJson(str) {
+	println(str)
+	def jsonSlurper = new JsonSlurper()
+	def object = jsonSlurper.parseText str
+	object = object.findAll{ it.value % 9 == 0 }
+	jsonObject = JsonOutput.toJson(object)
+}
+
+#println(parseAndFilterJson("{\"Kate\":18,\"Alan\":16,\"Osvald\":27}"))
+
+return this
