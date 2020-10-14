7a55ff5bb3db9973b6d11ce31e351205e2871e99
supreme8888 <61455911+supreme8888@users.noreply.github.com>
2020-10-14 16:55:13 +0300

    Update task9.groovy

--- tasks/task9.groovy
+++ tasks/task9.groovy
@@ -1,7 +1,7 @@
 def exec(string){
     def command_exec = ""
     command_exec = string.execute()
-    string = command_exec.trim()
+    string = command_exec.tesx.trim()
     return string
 }
 return this
