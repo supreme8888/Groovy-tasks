02b0a26cfff3523c378213bc9390fed2d17fd790
Valiantsin Ratomski <Valiantsin_Ratomski@epam.com>
2020-10-14 22:15:56 +0300

    added tasks

---
+++ tasks/calculator.groovy
@@ -0,0 +1,78 @@
+def intermediateValue(brac) {
+  a = (int)brac[0] - 48
+  op = brac[1]
+  b = (int)brac[2] - 48
+  
+  switch(op) {
+    case '-': return a - b; break
+    case '+': return a + b; break
+    case '*': return a * b; break
+    case '/': return a.intdiv(b); break
+  }
+}
+
+def exec(str) {
+  brac = str.find(/\(([^)]*)\)/)
+  brac_txt = brac
+
+  if(brac) {
+  brac = brac.split("")[1..-2]
+  str = str.replace(brac_txt, (String)intermediateValue(brac))
+  }
+   
+  str = (List)str.split("")
+
+  println str
+
+  for (i = 0; i < str.size(); i++) {
+    switch(str[i]) {
+      case '*':
+        ind = str.indexOf('*')
+        tmp = ((int)str[ind-1] - 48) * ((int)str[ind+1]-48)
+        str[i] = (String)tmp
+        str[ind-1] = 'del'
+        str[ind+1] = 'del'
+        i++
+        break
+      case '/':
+        ind = str.indexOf('/')
+        tmp = ((int)str[ind-1] - 48) / ((int)str[ind+1]-48)
+        str[i] = (String)tmp
+        str[ind-1] = 'del'
+        str[ind+1] = 'del'
+        i++
+        break
+    }
+  }
+    
+  str.removeAll('del')
+  println str
+  
+  for (i = 0; i < str.size(); i++) {
+    switch(str[i]) {
+      case '+':
+        ind = str.indexOf('+')
+        tmp = ((int)str[ind-1] - 48) + ((int)str[ind+1] - 48)
+        str[i] = (String)tmp
+        str[ind-1] = 'del'
+        str[ind+1] = 'del'
+        i++
+        break
+      case '-':
+        ind = str.indexOf('-')
+        tmp = ((int)str[ind-1] - 48) - ((int)str[ind+1] - 48)
+        str[i] = (String)tmp
+        str[ind-1] = 'del'
+        str[ind+1] = 'del'
+        i++
+        break
+    }
+  }
+    
+  str.removeAll('del')
+  println str.join()
+}
+
+exec("6*(3+1)")
+
+return this
