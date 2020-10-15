92dd07cb79a9709a98f23c097d66a3ccd8a2248b
Iryna Zaitsava <irinabro85@gmail.com>
2020-10-15 09:00:05 +0300

    fix : change code for test

--- tasks/task2.groovy
+++ tasks/task2.groovy
@@ -1,11 +1,13 @@
 def mulEvenOdd(List list){
   for (int i=0; i < list.size(); i++)
   {
-    if (list[i]%2 == 0)
+    if (list[i]%2 == 0) {
       list[i] = list[i]*2;
-    if (list[i]%2 == 1)
+      }
+    else {
       list[i] = list[i]*3;
     }
+   }
 return list
 }
 
